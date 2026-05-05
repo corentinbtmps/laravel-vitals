9926b50bde054fc385d48bed241bfa284952e25a
corentinbtmps <corentin@humantocomputer.com>
2026-05-05 23:24:39 +0200

    feat: add VitalsServiceProvider scaffold

---
+++ src/VitalsServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaravelVitals;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+/**
+ * Vitals package service provider.
+ *
+ * Wiring of config, migrations, views, routes, commands and assets is
+ * delegated to spatie/laravel-package-tools' fluent registration API.
+ */
+final class VitalsServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name('laravel-vitals')
+            ->hasConfigFile('vitals');
+        // Migrations, views, routes, commands and translations are added
+        // in subsequent tasks of this plan as their files come into existence.
+    }
+}
